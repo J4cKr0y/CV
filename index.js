ad5832d8db37606994ea7ea4e6309853ee4c4db2
J4cKr0y <Kiky270281@gmail.com>
2025-04-19 16:19:32 +0200

    feat: add hidden button to download PDF

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ const observer = new IntersectionObserver((entries) => {
             rootMargin: '0px'
         });
         document.querySelectorAll('.animate').forEach((el) => observer.observe(el));
+
+document.getElementById('downloadPDF').addEventListener('click', function () {
+    const element = document.body; 
+    const options = {
+        margin: 1,
+        filename: 'CV_Jacky.pdf',
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    };
+    html2pdf().set(options).from(element).save();
+});
